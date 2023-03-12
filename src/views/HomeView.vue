54b75532c2c3aa15c79c3cb9e7dd87c0a9e78e52
GregoryKogan <grisha.koganovskiy@gmail.com>
2023-03-12 17:23:02 +0300

    boilerplate removed

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,9 +1,7 @@
-<script setup lang="ts">
-import TheWelcome from '../components/TheWelcome.vue'
-</script>
-
 <template>
   <main>
-    <TheWelcome />
+    <div class="home">
+      <h1>Hello, world!</h1>
+    </div>
   </main>
 </template>
